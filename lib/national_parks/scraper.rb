6ae5c300c23f0cf928a992b0d270dc292ca2892f
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-06-10 18:51:37 +0000

    problems solved after checking out old commit. creating new branch.

--- lib/national_parks/scraper.rb
+++ lib/national_parks/scraper.rb
@@ -6,16 +6,12 @@ class NationalParks::Scraper
     parks_title = page.css(".post-title")
   end
 
-  # def make_new_parks
-  #   park_scraper.each do |a|
-  #     new = NationalParks::Park.new_from_index_page(a)
-  #     binding.pry
-  #   end
-  # end
-
-  def make_new_parks
-
-  end
+   def make_new_parks
+     park_scraper.each do |a|
+       new = NationalParks::Park.new_from_index_page(a)
+       binding.pry
+     end
+   end
 
   def self.details
     doc = Nokogiri::HTML(open("https://www.national-park.com/welcome-to-yosemite-national-park/"))
