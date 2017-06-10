7e25a390aef0868e7a002a26481d5f446237212d
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-06-10 18:42:46 +0000

    Something weird going on with Scraping class

--- lib/national_parks/scraper.rb
+++ lib/national_parks/scraper.rb
@@ -4,24 +4,17 @@ class NationalParks::Scraper
   def park_scraper
     page = Nokogiri::HTML(open("https://www.national-park.com/category/parks/"))
     parks_title = page.css(".post-title")
-  end
 
-  # def make_new_parks
-  #   park_scraper.each do |a|
-  #     new = NationalParks::Park.new_from_index_page(a)
-  #     binding.pry
-  #   end
-  # end
+    all_park_titles = []
+    parks_title.each do |park|
+       all_park_titles << park.text.strip.
+    end #each
+    all_park_titles.drop(1)
+  end #park_scraper
 
   def make_new_parks
-
-  end
-
-  def self.details
-    doc = Nokogiri::HTML(open("https://www.national-park.com/welcome-to-yosemite-national-park/"))
-    titles = doc.css("strong")
-  end
-
+    park_scraper.each do |a|
+      new = NationalParks::Park.new_from_index_page(a)
+    end #each
+  end #make_new_parks
 end
-
-#parks_url = page.css(".post-title a").attr("href").value
