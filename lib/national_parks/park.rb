78e455321d58bbbe12576b0fddfdf23edf92bf71
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-06-10 10:09:07 +0000

    Add highlight method in Park class that can scrape hightlights of the park, add doc method

--- lib/national_parks/park.rb
+++ lib/national_parks/park.rb
@@ -1,9 +1,22 @@
 class NationalParks::Park
-  attr_accessor :name, :location, :highlight, :url
+  attr_accessor :name, :location, :highlight, :url, :doc
 
-   def initialize(name, highlight)
-     @name = name
-     @highlight = highlight
-   end
+  @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def doc
+    @doc = Nokogiri::HTML(open("https://www.national-park.com/category/parks/"))
+  end
+
+  def highlight
+    @highlight = doc.css(".entry p").last.text.strip
+  end
 end
--- lib/national_parks/scraper.rb
+++ lib/national_parks/scraper.rb
@@ -3,18 +3,21 @@ class NationalParks::Scraper
 
   def self.park_scraper
     page = Nokogiri::HTML(open("https://www.national-park.com/category/parks/"))
-    parks_title = page.css(".post-title").first.text
-    parks_highlight = page.css(".entry p").first.text
-    #parks_url = page.css(".post-title a").attr("href").value
-    new_park = NationalParks::Park.new(parks_title, parks_highlight)
+    parks_title = page.css(".post-title")
+
+    new_park = []
+
+    parks_title.each do |title|
+      new_park << NationalParks::Park.new(title.text.strip)
+    end
     binding.pry
   end
 
   def self.details
     doc = Nokogiri::HTML(open("https://www.national-park.com/welcome-to-yosemite-national-park/"))
     titles = doc.css("strong")
-    binding.pry
   end
 
-
 end
+
+#parks_url = page.css(".post-title a").attr("href").value
